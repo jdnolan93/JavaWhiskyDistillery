acf1946bfecadb6cac5015f3cf917617078b3acf
mirkiy <mirizachar@gmail.com>
2022-02-23 14:34:34 +0000

    first two tests passed

---
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,43 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canFindWhiskyByYear(){
+		List<Whisky> foundWhisky = whiskyRepository.findWhiskyByYear(2018);
+		assertEquals(6, foundWhisky.size());
+		assertEquals("The Glendronach Revival", foundWhisky.get(0).getName());
+	}
+
+	@Test
+	public void canFindDistilleryByRegion(){
+		List<Distillery> foundDistillery= distilleryRepository.findDistilleryByRegion("Highland");
+		assertEquals(3,foundDistillery.size());
+	}
+}
