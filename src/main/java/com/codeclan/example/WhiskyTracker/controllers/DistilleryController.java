7c3d2a1cdaffc9a2fae227be7abb48653613aba0
mirkiy <mirizachar@gmail.com>
2022-02-23 15:18:45 +0000

    getMapping by Id

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,6 +25,11 @@ public class DistilleryController {
 
     public ResponseEntity<List<Distillery>> getAllDistilleries(){
         return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/distilleries/{id}")
+    public ResponseEntity getDistillery(@PathVariable Long id){
+        return new ResponseEntity<>(distilleryRepository.findById(id), HttpStatus.OK);
     }
 
 
